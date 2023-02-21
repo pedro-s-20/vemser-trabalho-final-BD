3b9718a01b32dede418150390e10624235010cd4
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-21 20:58:40 -0300

    formatação

--- bd/criacao.sql
+++ bd/criacao.sql
@@ -13,63 +13,66 @@ GRANT SELECT ANY TABLE TO TRAB_FINAL;
 
 CREATE TABLE ENDERECO (
 	id_endereco NUMBER(10) PRIMARY KEY,
-	logradouro varchar2(255) NOT null,
-	cep char(9) NOT NULL,
-	numero number(8) NOT NULL,
-	complemento varchar2(100)			
+	estado VARCHAR2(40) NOT NULL,
+	cidade VARCHAR2(100) NOT NULL,
+	bairro VARCHAR2(50) NOT NULL, 
+	logradouro VARCHAR2(255) NOT null,
+	cep CHAR(9) NOT NULL,
+	numero NUMBER(8) NOT NULL,
+	complemento VARCHAR2(100)			
 )
 
 CREATE TABLE CONTATO (
 	id_contato NUMBER(10) PRIMARY KEY,
-	telefone1 varchar2(11) NOT NULL,
-	telefone2 varchar2(11),
-	telefone3 varchar2(11)
+	telefone1 VARCHAR2(11) NOT NULL,
+	telefone2 VARCHAR2(11),
+	telefone3 VARCHAR2(11)
 );
 
 CREATE TABLE USUARIO (
 	id_usuario NUMBER(10) PRIMARY KEY,
+	cpf CHAR(11) UNIQUE NOT NULL,
+	email VARCHAR2(300) NOT NULL UNIQUE,
+	nome VARCHAR2(255) NOT NULL,
+	senha VARCHAR2(300) NOT NULL,
 	id_endereco NUMBER(10),
 	id_contato NUMBER(10),
-	nome varchar2(255) NOT NULL,
-	cpf char(11) UNIQUE NOT NULL,
-	email varchar2(300) NOT NULL UNIQUE,
-	senha varchar2(300) NOT NULL,
 	CONSTRAINT FK_USARIO_CONTATO FOREIGN KEY (id_contato) REFERENCES Contato (id_contato),
 	CONSTRAINT FK_USARIO_ENDERECO FOREIGN KEY (id_endereco) REFERENCES ENDERECO (id_endereco)	
 );
 
 CREATE TABLE ADMINISTRATIVO (
 	id_administrativo NUMBER(10) PRIMARY KEY,
-	id_usuario number(10) NOT NULL,
+	id_usuario NUMBER(10) NOT NULL,
 	CONSTRAINT FK_ADMINISTRATIVO_USUARIO FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario)
 )
 
 CREATE TABLE CONVENIO (
-	id_convenio number(10) PRIMARY KEY,
-	cadastro_orgao_regulador varchar2(40) NOT NULL,
-	taxa_abatimento decimal(2,4) NOT NULL
+	id_convenio NUMBER(10) PRIMARY KEY,
+	cadastro_orgao_regulador VARCHAR2(40) NOT NULL,
+	taxa_abatimento DECIMAL(2,4) NOT NULL
 )
 
 CREATE TABLE CLIENTE (
-	id_cliente number(10) PRIMARY KEY,	
+	id_cliente NUMBER(10) PRIMARY KEY,	
 	id_convenio NUMBER(10),	
-	id_usuario number(10) NOT null,
+	id_usuario NUMBER(10) NOT null,
 	CONSTRAINT FK_CLIENTE_USUARIO FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario),
 	CONSTRAINT FK_CLIENTE_CONVENIO FOREIGN KEY (id_convenio) REFERENCES Convenio (id_convenio)
 )
 
 CREATE TABLE ESPECIALIDADE (
 	id_especialidade NUMBER(10) PRIMARY KEY,
-	nome varchar2(60) NOT NULL,
-	valor decimal(10,2) NOT NULL
+	valor DECIMAL(10,2) NOT NULL
+	nome VARCHAR2(60) NOT NULL,
 )
 
 
 CREATE TABLE MEDICO (
 	id_medico NUMBER(10) PRIMARY KEY,
-	id_usuario number(10) NOT NULL,
-	id_especialidade number(10) NOT NULL,
-	crm varchar2(13) UNIQUE NOT NULL,		
+	crm VARCHAR2(13) UNIQUE NOT NULL,
+	id_especialidade NUMBER(10) NOT NULL,
+	id_usuario NUMBER(10) NOT NULL,
 	CONSTRAINT FK_MEDICO_USUARIO FOREIGN KEY (id_usuario) REFERENCES Usuario (id_usuario),
 	CONSTRAINT FK_MEDICO_ESPECIALIDADE FOREIGN KEY (id_especialidade) REFERENCES Especialidade (id_especialidade)
 )
@@ -77,11 +80,11 @@ CREATE TABLE MEDICO (
 
 CREATE TABLE AGENDAMENTO (
 	id_agendamento NUMBER(10) PRIMARY KEY,	
-	id_medico number(10) NOT NULL,
-	id_cliente number(10) NOT NULL,
+	id_cliente NUMBER(10) NOT NULL,
+	id_medico NUMBER(10) NOT NULL,
+	tratamento VARCHAR2(40),
+	exame VARCHAR2(40),
 	data_horario DATE NOT NULL,
-	exame varchar2(40),
-	tratamento varchar2(40),
 	CONSTRAINT FK_AGENDAMENTO_MEDICO FOREIGN KEY (id_medico) REFERENCES MEDICO (id_medico),
 	CONSTRAINT FK_AGENDAMENTO_CLIENTE FOREIGN KEY (id_cliente) REFERENCES CLIENTE (id_cliente)
 )
