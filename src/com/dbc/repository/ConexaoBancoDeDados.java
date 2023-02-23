caf3072ec3cdf9edf4b5e6208b0c44fbcaa01199
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-23 09:03:53 -0300

    banco de dados

--- src/com/dbc/repository/ConexaoBancoDeDados.java
+++ src/com/dbc/repository/ConexaoBancoDeDados.java
@@ -5,14 +5,14 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBancoDeDados {
-    private static final String SERVER = "localhost";
-    private static final String PORT = "1521"; // Porta TCP padrão do Oracle
+    private static final String SERVER = "vemser-dbc.dbccompany.com.br";
+    private static final String PORT = "25000"; // Porta TCP padrão do Oracle
     private static final String DATABASE = "xe";
 
     // Configuração dos parâmetros de autenticação
-    private static final String USER = "system";
-    private static final String PASS = "oracle";
-    private static final String SCHEMA = "VEM_SER";
+    private static final String USER = "SAUDE";
+    private static final String PASS = "ZKcUQpyWvUto";
+    private static final String SCHEMA = "SAUDE";
 
     public static Connection getConnection() throws SQLException {
         String url = "jdbc:oracle:thin:@" + SERVER + ":" + PORT + ":" + DATABASE;
