18198f1bf3a1b443b65d8e7b57253619e41b384c
Matheus Palermo <89110918+matheus1629@users.noreply.github.com>
2023-02-26 16:20:50 -0300

    wip

--- src/com/dbc/view/ValorEntrada.java
+++ src/com/dbc/view/ValorEntrada.java
@@ -2,6 +2,8 @@ package com.dbc.view;
 
 import com.dbc.exceptions.ValorDeEntradaException;
 
+import java.math.BigDecimal;
+
 public class ValorEntrada {
 
     // validar int
@@ -32,25 +34,35 @@ public class ValorEntrada {
 
     }
 
-    // validar entrada double
-    public static void validarEntrada(Double taxa) {
-        if (taxa < 0 || taxa > 100) {
-            throw new ValorDeEntradaException("Taxa fora do permitido: " + taxa + ". O número da taxa precisa ser entre " + 0 + " e " + 100 + ".");
-        }
-        if (taxa != null) {
-            if (taxa > 99) {
-                System.err.println("Erro em 'Taxa de Abatimento': Número inválido como taxa (%)!");
-            }
+//    public static void validarEntrada(Double taxa) {
+//        if (taxa < 0 || taxa > 100) {
+//            throw new ValorDeEntradaException("Taxa fora do permitido: " + taxa + ". O número da taxa precisa ser entre " + 0 + " e " + 100 + ".");
+//        }
+//        if (taxa != null) {
+//            if (taxa > 99) {
+//                System.err.println("Erro em 'Taxa de Abatimento': Número inválido como taxa (%)!");
+//            }
+//
+//            Double valorDeEntrada = taxa;
+//            String valorComCasasDividas[] = valorDeEntrada.toString().split("\\.");
+//
+//            if (valorComCasasDividas[0].length() > 2 || (valorComCasasDividas.length == 2 && valorComCasasDividas[1].length() > 2)) {
+//                throw new ValorDeEntradaException("Caracteres inserido número incorreto: " + taxa + ". A taxa precisa ter 4 casas, sendo duas decimais.");
+//            }
+//        }
+//
+//    }
 
-            Double valorDeEntrada = taxa;
+    public static void validarEntrada(Double valor, int casasAntesDaVirgula, int casasAposDaVirgula) {
+            BigDecimal valorDeEntrada = new BigDecimal(valor).setScale(2, BigDecimal.ROUND_UP);
             String valorComCasasDividas[] = valorDeEntrada.toString().split("\\.");
 
-            if (valorComCasasDividas[0].length() > 2 || (valorComCasasDividas.length == 2 && valorComCasasDividas[1].length() > 2)) {
-                throw new ValorDeEntradaException("Caracteres inserido número incorreto: " + taxa + ". A taxa precisa ter 4 casas, sendo duas decimais.");
+            if (valorComCasasDividas[0].length() > casasAntesDaVirgula || (valorComCasasDividas.length == 2 && valorComCasasDividas[1].length() > casasAposDaVirgula)) {
+                throw new ValorDeEntradaException("Caracteres de número inserido incorreto: " + valorDeEntrada + ". O valor precisa ter "+ (casasAntesDaVirgula + casasAposDaVirgula) + " casas, sendo " + casasAposDaVirgula +" decimais.");
             }
         }
 
-    }
+
 
 
 }
